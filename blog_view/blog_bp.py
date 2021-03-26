b1e32350f28fcf3350a1267b85f3b49daccb1d82
divertome <hans9645@gmail.com>
2021-03-26 17:59:17 +0900

    add contents and change file name, blue print's code

--- blog_view/blog_bp.py
+++ blog_view/blog_bp.py
@@ -2,6 +2,10 @@ from flask import Flask,Blueprint,request,render_template
 
 blog_abtest=Blueprint('blog',__name__)
 
-@blog_abtest.route('/test')
-def test():
-    return render_template("blog_A.html")
+@blog_abtest.route('/eng')
+def eng():
+    return render_template("blog_eng.html")
+
+@blog_abtest.route('/kor')
+def kor():
+    return render_template("blog_kor.html")
