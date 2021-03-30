b4164206160c13f34c1b132ca922ef06fee1929a
divertome <hans9645@gmail.com>
2021-03-30 13:38:53 +0900

    fix wrong code

---
+++ blog_control/session_mgmt.py
@@ -0,0 +1,33 @@
+from db_model.mongodb import conn_mongodb
+from datetime import datetime
+
+class BlogSession():
+    blog_page={'E':'blog_eng.html','K':'blog_kor.html'}
+    session_count=0
+
+    @staticmethod
+    def save_session_info(session_ip,user_email, webpage_name):
+                            #접속 때마다 접속정보를 mongodb에 저장하는 함수
+        now =datetime.now()
+        now_time=now.strftime("%d-%m-%Y %H:%M:%S") #https:strftime.org
+
+        mongo_db=conn_mongodb()
+        mongo_db.insert_one({
+            'session_ip':session_ip,
+            'user_email':user_email,
+            'page':webpage_name,
+            'access_time':now_time
+            })
+
+    
+    @staticmethod
+    def get_blog_page(blog_id=None):#인자를 안넣어도 되고 넣으면 사용가능하게 하는 문법.
+        if blog_id==None:
+            if BlogSession.session_count==0:
+                BlogSession.session_count=1
+                return 'blog_engA.html'
+            else:
+                BlogSession.session_count=0
+                return 'blog_engB.html'
+        else:
+            return BlogSession.blog_page[blog_id]
