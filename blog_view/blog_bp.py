13ac672e818c08b1a700b83c43449cd6875dd41a
divertome <hans9645@gmail.com>
2021-03-30 15:39:18 +0900

    did

--- blog_view/blog_bp.py
+++ blog_view/blog_bp.py
@@ -1,11 +1,25 @@
-from flask import Flask,Blueprint,request,render_template
+from flask import Flask,Blueprint,request,render_template,redirect,make_response,jsonify,url_for
 
-blog_abtest=Blueprint('blog',__name__)
+blog_abtest=Blueprint('blog_bp',__name__)
 
 @blog_abtest.route('/engA')
-def eng():
+def engA():
     return render_template("blog_engA.html")
 
 @blog_abtest.route('/engB')
-def kor():
+def engB():
     return render_template("blog_engB.html")
+
+
+@blog_abtest.route('/set_email',methods=["GET","POST"])
+def set_email():
+    if request.method=='GET':
+        print("set_email()",request.args.get('user_email'))
+        #return make_response(jsonify(success=True),200) 
+        #->vue와 같은 js를 사용하지 않아서 서버단에서 풀스택을 구현하는 거라 전체페이지를 리로딩하는 식으로한다.
+        #return redirect(url_for('blog_bp.engA'))
+        return redirect('/blueprint/engA')
+    else:
+        print('set_email',request.get_json())
+        #post방식으로 데이터를 가져올 수 있다. vue에서 했었음
+        return redirect(url_for('blog_bp.engA'))
