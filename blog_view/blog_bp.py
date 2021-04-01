c0e2405a521cf3a7ca9a66ea6e32233d7a4dc1d6
divertome <hans9645@gmail.com>
2021-04-01 21:59:18 +0900

    add login user log func

--- blog_view/blog_bp.py
+++ blog_view/blog_bp.py
@@ -20,10 +20,12 @@ def engB():
 
 
 @blog_abtest.route('/fullstack')
-def fullstack():
-    
+def fullstack():    
     if current_user.is_authenticated:#세션확인 후 구독이력 확인
-        return render_template("blog_engA.html",user_email=current_user.user_email)#여기에 jinja2에 들어갈 변수를 같이 넣어준다.
+        web_page=BlogSession.get_blog_page(current_user.blog_id)#구독시 페이지정보  
+        BlogSession.save_session_info(session['client_id'],current_user.user_email,web_page)
+        #로그인한 방문자의 IP등을 세션라이브러리로부터 받아와서 넣는다.      
+        return render_template(web_page,user_email=current_user.user_email)#여기에 jinja2에 들어갈 변수를 같이 넣어준다.
     else:
         web_page=BlogSession.get_blog_page()
         BlogSession.save_session_info(session['client_id'],'anonymous',web_page)
@@ -52,11 +54,11 @@ def set_email():
         #http request의 헤더부분을 가져올 수 있다. 이후 content type를 확인하고 데이터를 어떻게 가져와야하는지 고민하자.
         #content type이 application/json인 경우에는 request.get_json()
         #->print('set_email',request.get_json())
-        print('set_email',request.form)
-        print('only user_email',request.form['user_email'])
+        #print('blog_id',request.form['blog_id'])
+        #print('only user_email',request.form['user_email'])
         #post방식으로 데이터를 가져올 수 있다. vue에서 했었음
-        user=User.create(request.form['user_email'],'A')
+        user=User.create(request.form['user_email'],request.form['blog_id'])
         login_user(user,remember=True, duration=datetime.timedelta(days=30))
         #로그인 기록유지 시킴 remember me,flask_login google검색
         #세션정보가 플라스크에서 만들어져 셋쿠키로 웹브라우저에 전송, 웹브라우저는 서버주소와 쿠키를 저장,관리를 하면서 다음에 해당서버에 request를 할 때 사용한다. 
-        return redirect(url_for('blog_bp.engA'))
+        return redirect(url_for(request.form['blog_id']))
